d6237415177e73bfdeb956bbd0c690892151ddb1
Levan <levan@linux-063v.DLink>
2017-02-21 23:12:31 +0200

    lab3/ advanced task

--- Lab3/task2.c
+++ Lab3/task2.c
@@ -2,29 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <ctype.h>
 #define INPUT_FILE "data_short.txt"
 
 
 #define MAX 20
 #define MAX_NUMBER 220
 
-struct employee{
+typedef struct employee{
 	int id;
 	char first[MAX];
 	char last[MAX];
 	char city[MAX];
-};
+}employee;
 
-struct place{
-	char member[210][20];
+typedef struct place{
+	char member[MAX_NUMBER][20];
 	char name[20];
 	int size;
-};
+}place;
+
+void sort(employee staff[],int number);
+void partialSearch(place cities[],int number);
+int strcmpi(char* s1, char* s2);
 
 int main(void){
-	struct employee staff[MAX_NUMBER];
-	struct place cities[MAX_NUMBER];
+	employee staff[MAX_NUMBER];
+	place cities[MAX_NUMBER];
 	int i=0;
 	int numberOfCities=0;
 	char choice[MAX];
@@ -65,20 +69,21 @@ int main(void){
 	
 	//output
 	/**for(i=0;i<numberOfCities;i++){
-		printf("City %s has %d members\n",cities[i].name,cities[i].size);
+		printf("City %s has %d members\n",cities[i].name,cities[i].size+1);
 	}
 	*/
+	
 	printf("Enter city name: ");
 	scanf("%s",choice);
 	int index=-1;
 	for(i=0;i<numberOfCities;i++){
-		if(strcmp(choice,cities[i].name)==0){
+		if(strcmpi(choice,cities[i].name)==0){
 			index=i;
 			break;
 		}
 	}
 	if(index>=0){
-		printf("City %s has %d members\n",cities[index].name,cities[index].size);
+		printf("City %s has %d members\n",cities[index].name,cities[index].size+1);
 		for(i=0;i<cities[index].size;i++){
 			printf("%s ",cities[index].member[i]);
 		}
@@ -86,6 +91,38 @@ int main(void){
 		printf("City %s could not be found\n",choice);
 	}
 	
+	partialSearch(cities,numberOfCities);
 	return 0;
 	
+}
+
+void sort(employee staff[],int number){
+	
+}
+void partialSearch(place cities[],int number){
+	char choice[MAX];
+	int i;
+	int count=0;
+	printf("Partial search allowed\n");
+	printf("Enter city name: ");
+	scanf("%s",choice);
+	for(i=0;i<number;i++){
+		if(strstr(cities[i].name,choice)!=NULL){
+			printf("City %s has %d members\n",cities[i].name,cities[i].size+1);
+			count++;
+		}
+	}
+	printf("Match from total: %d/%d\n",count,number);
+}
+int strcmpi(char* s1, char* s2){
+    int i;
+     
+    if(strlen(s1)!=strlen(s2))
+        return -1;
+         
+    for(i=0;i<strlen(s1);i++){
+        if(toupper(s1[i])!=toupper(s2[i]))
+            return s1[i]-s2[i];
+    }
+    return 0;
 }
